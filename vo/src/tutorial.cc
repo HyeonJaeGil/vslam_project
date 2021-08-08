4fcdd0d2ad2f30e88518db389623714ba5de31ff
HyeonJaeGil <now9728@kaist.ac.kr>
2021-08-09 01:12:48 +0900

    sharpen images with filter and handwritten function and calculate time with TicCount

--- vo/src/tutorial.cc
+++ vo/src/tutorial.cc
@@ -1,6 +1,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 #include <vector>
 
@@ -8,51 +9,75 @@ using std::cout;
 using std::endl;
 
 using namespace cv;
-int main()
+
+/*
+[Result-Debug Mode]
+Hand written function time passed in seconds: 0.00701735
+Built-in filter2D time passed in seconds:     0.0101438
+
+[Result-Release Mode]
+Hand written function time passed in seconds: 0.00233336
+Built-in filter2D time passed in seconds:     0.00104528
+*/
+
+void Sharpen(const Mat& myImage,Mat& Result);
+
+
+int main( int argc, char* argv[])
 {
-    Mat M(200,200, CV_8UC3, Scalar(0,0,255)); // BGR ordered.
-    imshow("Display window", M);
-    int k = waitKey(0); // Wait for a keystroke in the window
+    Mat src, dst0, dst1;
+    if (argc >= 3 && !strcmp("G", argv[2]))
+        src = imread( "/home/hj/Data/frame_2.png", IMREAD_GRAYSCALE);
+    else
+        src = imread( "/home/hj/Data/frame_2.png", IMREAD_COLOR);
+    if (src.empty())
+    {
+        std::cerr << "Can't open image " << std::endl;
+        return EXIT_FAILURE;
+    }
+    namedWindow("Input", WINDOW_AUTOSIZE);
+    namedWindow("Output", WINDOW_AUTOSIZE);
+    imshow( "Input", src );
 
-
-    // channels, dimensions, depth
-    int sz[3] = {20,10,3};
-    Mat L(3,sz, CV_8UC(1), Scalar::all(0));
-    cout << " L channels: " << L.channels() << " L dims: " << L.dims << " L depth: " << L.depth() << endl;
-
-
-    // reshape and channel numbers
-    Mat C = (Mat_<double>(3,3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
-    cout << "C = " << endl << " " << C.channels() << endl << endl; 
-    cout << "C (numpy)   = " << endl << format(C, Formatter::FMT_NUMPY ) << endl << endl;
-
-    C = (Mat_<double>({0, -1, 0, -1, 5, -1, 0, -1, 0})).reshape(3,3);
-    cout << "C = " << endl << " " << C.channels() << endl << endl;
-    cout << "C (numpy)   = " << endl << format(C, Formatter::FMT_NUMPY ) << endl << endl;
-
-    C = (Mat_<double>({0, -1, 0, -1, 5, -1, 0, -1, 0}));
-    cout << "C = " << endl << " " << C.channels() << endl << endl;
-
-
-    // Take a portion of a Mat and make new Mat
-    Mat RowClone = C.row(1).clone();
-    cout << "RowClone = " << endl << " " << RowClone << endl << endl;
-
-
-    // show in different format
-    Mat R = Mat(3, 3, CV_8UC3);
-    randu(R, Scalar::all(0), Scalar::all(255));
-    cout << "R = " << endl << " " << R << endl << endl;
-    cout << "R (csv)     = " << endl << format(R, Formatter::FMT_CSV   ) << endl << endl;
-    cout << "R (numpy)   = " << endl << format(R, Formatter::FMT_NUMPY ) << endl << endl;
-
-
-    // cout vectors via Mat
-    std::vector<float> v;
-    v.push_back( (float)CV_PI);   v.push_back(2);    v.push_back(3.01f);
-    cout << "Vector of floats via Mat = " << endl << Mat(v) << endl << endl;
-    // cout << "Vector of floats = " << endl << v << endl << endl; // error: vector does not have << operator
+    double t = (double)getTickCount();
+    Sharpen( src, dst0 );
+    t = ((double)getTickCount() - t)/getTickFrequency();
+    cout << "Hand written function time passed in seconds: " << t << endl;
+    imshow( "Output", dst0 );
+    waitKey();
     
-    return 0;
+    Mat kernel = (Mat_<char>(3,3) <<  0, -1,  0,
+                                   -1,  5, -1,
+                                    0, -1,  0);
+    t = (double)getTickCount();
+    filter2D( src, dst1, src.depth(), kernel );
+    t = ((double)getTickCount() - t)/getTickFrequency();
+    cout << "Built-in filter2D time passed in seconds:     " << t << endl;
+    imshow( "Output", dst1 );
+    waitKey();
+    return EXIT_SUCCESS;
+}
 
+
+void Sharpen(const Mat& myImage,Mat& Result)
+{
+    CV_Assert(myImage.depth() == CV_8U);  // accept only uchar images
+    const int nChannels = myImage.channels();
+    Result.create(myImage.size(),myImage.type());
+    for(int j = 1 ; j < myImage.rows-1; ++j)
+    {
+        const uchar* previous = myImage.ptr<uchar>(j - 1);
+        const uchar* current  = myImage.ptr<uchar>(j    );
+        const uchar* next     = myImage.ptr<uchar>(j + 1);
+        uchar* output = Result.ptr<uchar>(j);
+        for(int i= nChannels;i < nChannels*(myImage.cols-1); ++i)
+        {
+            *output++ = saturate_cast<uchar>(5*current[i]
+                         -current[i-nChannels] - current[i+nChannels] - previous[i] - next[i]);
+        }
+    }
+    Result.row(0).setTo(Scalar(0));
+    Result.row(Result.rows-1).setTo(Scalar(0));
+    Result.col(0).setTo(Scalar(0));
+    Result.col(Result.cols-1).setTo(Scalar(0));
 }
